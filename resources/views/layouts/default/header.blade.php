86dd438bf672f9859928b3d66e7a8e4515873efe
jkdalton <jordan.dalton@ymail.com>
2015-01-19 22:52:42 -0600

    Changed to navbar-right

--- resources/views/layouts/default/header.blade.php
+++ resources/views/layouts/default/header.blade.php
@@ -1,4 +1,3 @@
-<!-- Static navbar -->
 <nav class="navbar navbar-inverse navbar-fixed-top">
     <div class="container">
         <div class="navbar-header">
@@ -17,33 +16,38 @@
                 <li class="{{ isActiveRouteName('posts.show_off') }}"><a href="{{ route('posts.show_off') }}">show</a></li>
                 <li class="{{ isActiveRouteName('posts.ask') }}"><a href="{{ route('posts.ask') }}">ask</a></li>
                 <li class="{{ isActiveRouteName('posts.create') }}"><a href="{{ route('posts.create') }}">submit</a></li>
+
+                {{-- Show if the user is logged in. --}}
                 @if( Auth::check() )
-                <li class="{{ isActiveRouteName('posts.submitted') }}"><a href="{{ route('posts.submitted') }}">your submissions</a></li>
-                <li class="{{ isActiveRouteName('comments.threads') }}"><a href="{{ route('comments.threads') }}">your threads</a></li>
+                    <li class="{{ isActiveRouteName('posts.submitted') }}"><a href="{{ route('posts.submitted') }}">your submissions</a></li>
+                    <li class="{{ isActiveRouteName('comments.threads') }}"><a href="{{ route('comments.threads') }}">your threads</a></li>
                 @endif
+
             </ul>
+
+            {{-- Show if the user is NOT logged in. --}}
             @if( ! Auth::check() )
-            <ul class="nav navbar-nav pull-right">
-                <li class="{{ isActiveRouteName('auth.login') }}"><a href="{{ route('auth.login') }}"><i class="fa fa-sign-in"></i> login</a></li>
-                <li class="{{ isActiveRouteName('auth.register') }}"><a href="{{ route('auth.register') }}"><i class="fa fa-arrow-right"></i> Join</a></li>
-            </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="{{ isActiveRouteName('auth.login') }}"><a href="{{ route('auth.login') }}"><i class="fa fa-sign-in"></i> login</a></li>
+                    <li class="{{ isActiveRouteName('auth.register') }}"><a href="{{ route('auth.register') }}"><i class="fa fa-arrow-right"></i> Join</a></li>
+                </ul>
             @else
-            <ul class="nav navbar-nav navbar-right">
-                <li class="dropdown">
-                    <a aria-expanded="false" role="button" data-toggle="dropdown" class="dropdown-toggle" href="#"><i class="fa fa-user"></i> {{ Auth::user()->username }} <span class="caret"></span></a>
-                    <ul role="menu" class="dropdown-menu">
-                        @if( $_is_admin_or_moderator )
-                            <li><a href="{{ route('admin.dashboard.index') }}"><i class="fa fa-dashboard"></i> Go to Admin Dashboard</a></li>
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="dropdown">
+                        <a aria-expanded="false" role="button" data-toggle="dropdown" class="dropdown-toggle" href="#"><i class="fa fa-user"></i> {{ Auth::user()->username }} <span class="caret"></span></a>
+                        <ul role="menu" class="dropdown-menu">
+                            @if( $_is_admin_or_moderator )
+                                <li><a href="{{ route('admin.dashboard.index') }}"><i class="fa fa-dashboard"></i> Go to Admin Dashboard</a></li>
+                                <li class="divider"></li>
+                            @endif
+                            <li><a href="{{ Auth::user()->present()->getEditProfileLink() }}"><i class="fa fa-cogs"></i> Settings</a></li>
                             <li class="divider"></li>
-                        @endif
-                        <li><a href="{{ Auth::user()->present()->getEditProfileLink() }}"><i class="fa fa-cogs"></i> Settings</a></li>
-                        <li class="divider"></li>
-                        <li><a href="{{ Auth::user()->present()->getProfileLink() }} "><i class="fa fa-user"></i> My Profile</a></li>
-                        <li class="divider"></li>
-                        <li><a href="{{ route('logout') }}"><i class="fa fa-sign-out"></i> Log out</a></li>
-                    </ul>
-                </li>
-            </ul>
+                            <li><a href="{{ Auth::user()->present()->getProfileLink() }} "><i class="fa fa-user"></i> My Profile</a></li>
+                            <li class="divider"></li>
+                            <li><a href="{{ route('logout') }}"><i class="fa fa-sign-out"></i> Log out</a></li>
+                        </ul>
+                    </li>
+                </ul>
             @endif
         </div><!--/.nav-collapse -->
     </div><!--/.container-fluid -->
